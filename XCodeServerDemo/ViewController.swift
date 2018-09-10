41c276a477420231f921cebdb14eadcb1402f740
WangGuofeng <wangguofeng@live.cn>
2018-09-10 17:35:20 +0800

    初始化

---
+++ XCodeServerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XCodeServerDemo
+//
+//  Created by bdkj on 2018/9/10.
+//  Copyright © 2018年 bdkj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
